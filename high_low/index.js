1354bdc29699f0414358aadae9d8b33c35a91347
purplemiyazawa <58101308+grape4842@users.noreply.github.com>
2023-10-08 21:13:06 +0900

    Update index.js data add
    변환할 수 있는 문장 데이터 추가

--- high_low/index.js
+++ high_low/index.js
@@ -1,65 +1,79 @@
 var lis = [
+    [" 저는", " 나는"],
     ["입니다.", "이다."],
     ["합니다.", "한다."],
-    [" 저는", " 나는"],
-    ["했습니다.", "했다."],
-    ["봤습니다.", "봤다."],
-    ["쳤습니다.","쳤다."],
-    ["쌌습니다.","쌌다."],
-    ["해주세요.", "해요."],
-    ["샀습니다.","샀다."],
-    ["었습니다.","었다."],
-    ["먹습니다.","먹다."],
-    ["있습니다.", "있다."],
-    ["좋습니다.", "좋다."],
-    ["많습니다.", "많다."],
+    ["합니다.", "하다."],
     ["됩니다.", "된다."],
     ["없습니다.", "없다."],
-    ["하겠습니다.","하겠다."],
-    ["다가옵니다.","다가온다."],
-    ["필수입니다.","필수다."],
-    ["생깁니다.","생긴다."],
-    ["않습니다.","않는다."],
-    ["보겠습니다.","보겠다."],
-    ["하십니다.","하신다."],
-    ["가능합니다.","가능하다."],
-    ["하였습니다.", "하였다."],
-    ["중요합니다." , "중요하다."],
-    ["바랍니다.","바란다."],
-    ["나옵니다.","나온다."],
-    ["놓았습니다.", "놓았다."],
-    ["어렵습니다.", "어렵다."]
+    ["되었습니다.", "됐다."],
+    ["담습니다.", "담다."],
+    ["답니다.", "달다."],
+    ["듬습니다.", "듬다."],
+    ["옵니다.", "온다."],
+    ["했습니다.", "했다."],
+    ["십니다.", "신다."],
+    ["칩니다.", "치다."],
+    ["참습니다.", "참다."],
+    ["럽습니다.", "럽다."],
+    ["렵습니다.", "렵다."],
+    ["립니다.", "리다."],
+    ["렸습니다.", "렸다."],
+    ["겠습니다.", "겠다."],
+    ["봤습니다.", "봤다."],
+    ["쳤습니다.", "쳤다."],
+    ["쌌습니다.", "쌌다."],
+    ["샀습니다.", "샀다."],
+    ["셨습니다.", "셨다."],
+    ["었습니다.", "었다."],
+    ["웠습니다.", "웠다."],
+    ["였습니다.", "였다."],
+    ["았습니다.", "았다."],
+    ["있습니다.", "있다."],
+    ["먹습니다.", "먹다."],
+    ["좋습니다.", "좋다."],
+    ["싫습니다.", "싫다."],
+    ["씁니다.", "쓰다."],
+    ["십니다.", "시다."],
+    ["많습니다.", "많다."],
+    ["필수입니다.", "필수다."],
+    ["생깁니다.", "생긴다."],
+    ["않습니다.", "않는다."],
+    ["바랍니다.", "바란다."],
+    ["곱습니다.", "곱다."],
+    ["놓습니다.", "놓다."],
+    ["줍습니다.", "줍다."],
+    ["춥습니다.", "춥다."],
+    ["덥습니다.", "덥다."],
+    ["쉽습니다.", "쉽다."]
 ];
 
 
-
-
-function value_check(){
+function value_check() {
     var check_cnt = document.getElementsByName("lan").length;
-    for (var i=0; i<check_cnt;i++){
+    for (var i = 0; i < check_cnt; i++) {
         if (document.getElementsByName("lan")[0].checked == true) {
             low_high();
-            break        
-        }else if(document.getElementsByName("lan")[1].checked == true){
+            break
+        } else if (document.getElementsByName("lan")[1].checked == true) {
             high_low();
             break;
         }
-    }   
+    }
 }
 String.prototype.replaceAll = function(org, dest) {
     return this.split(org).join(dest);
 }
 
 //  입니다 이다 이다 입니다 한다 한다 합니다 합니다
-function low_high(){
-    for(var i=0; i<lis.length;i++){
-        resultString = resultString.replaceAll(lis[i][1],lis[i][0]);
+function low_high() {
+    for (var i = 0; i < lis.length; i++) {
+        resultString = resultString.replaceAll(lis[i][1], lis[i][0]);
     }
 }
 
-function high_low(){
-    for(var i=0; i<lis.length;i++){
-        resultString = resultString.replaceAll(lis[i][0],lis[i][1]);
+function high_low() {
+    for (var i = 0; i < lis.length; i++) {
+        resultString = resultString.replaceAll(lis[i][0], lis[i][1]);
     }
 }
 
@@ -67,4 +81,3 @@ function resize(obj) {
     obj.style.height = "1px";
     obj.style.height = 12 + obj.scrollHeight + "px";
 }
-
